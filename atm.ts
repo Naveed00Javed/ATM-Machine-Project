8028fb34558c382bad2d9a65608109ede25c4db8
Naveed Javed <163446043+Naveed00Javed@users.noreply.github.com>
2024-04-19 23:00:02 +0500

    ATM-Machine-Project

---
+++ atm.ts
@@ -0,0 +1,85 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+// initialize user balance and pin code
+let myBalance = 5000;
+let myPin = 1234;
+// print welcome message
+console.log(chalk.blue("\n\twelcome to code with Naveed - ATM Machine"));
+
+let pinAnswer = await inquirer.prompt([
+    {
+        name: "pin",
+        type: "number",
+        message: "Enter your pin code: "
+            
+        
+    }
+
+])
+if (pinAnswer.pin == myPin){
+    console.log("pin is correct, Login successfully!");
+    // console.log(`current Account Balance is ${myBalance}`)
+
+    let operationAns = await inquirer.prompt([
+        {
+            name: "operation",
+            type: "list",
+            message: "Select an operation:",
+            choices: ["withdraw Amount", "Check Balance"]
+        }
+    ])
+    if (operationAns.operation === "withdraw Amount"){
+        let withdrawAns = await inquirer.prompt([
+            {
+                name: "withdrawMethod",
+                type: "list",
+                message: "Select a withdraw Method:",
+                choices: ["Fast Cash", "Enter Amount"]
+            }
+        ])
+        if(withdrawAns.withdrawMethod === "Fast Cash"){
+            let fastCashAns = await inquirer.prompt([
+                {
+                    name: "fastCash",
+                    type: "list",
+                    message: "select Amount:",
+                    choices: [1000, 2000, 5000, 20000]
+                }
+            ])
+            if(fastCashAns.fastCash > myBalance){
+                console.log("Insufficient Balance!");
+            }
+            else{
+                myBalance -= fastCashAns.fastCash
+                console.log(`${fastCashAns} withdraw successfully`);
+                console.log(`Your remaining Balance is: ${myBalance}`)
+            }
+        }
+       else if(withdrawAns.withdrawMethod === "Enter Amount"){
+            let amountAns = await inquirer.prompt([
+                {
+                    name: "amount",
+                    type: "number",
+                    message: "Enter the amount to withdraw:"
+                }
+            ])
+            if(amountAns.amount > myBalance){
+                console.log("insufficient balance");
+            }
+            else{
+                myBalance -= amountAns.amount;
+                console.log(`${amountAns.amount} wihdraw Successfully`);
+                console.log(`your remaining balance is: ${myBalance}`)
+            }
+        }
+        
+
+    }
+    else if (operationAns.operation === "Check Balance"){
+        console.log(`Your Account Balance is: ${myBalance}`);
+    }
+}
+else{
+    console.log("pin is incorrect, try again!");
+}
